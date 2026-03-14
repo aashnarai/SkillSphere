3832cb322490f05e812d2e339be5a083c26466b1
aashnarai <aashnarai0@gmail.com>
2026-03-15 01:44:44 +0530

    Added user registration API

---
+++ backend/routes/auth.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  try {
+
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    await user.save();
+
+    res.json({ message: "User registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/", (req, res) => {
   res.send("SkillSphere API running");
 });
+const authRoutes = require("./routes/auth");
 
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
