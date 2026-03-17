824a882a4bb30ca40bd10432eeb4527dcfb99345
aashnarai <aashnarai0@gmail.com>
2026-03-18 01:25:12 +0530

    Added login page and connected API

---
+++ frontend/src/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { email, password }
+      );
+
+      console.log(res.data);
+
+      // Save token
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+
+      alert("Login successful");
+
+    } catch (err) {
+      alert("Login failed");
+      console.log(err);
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
